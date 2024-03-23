3cb0f52995bf0f4544426fbabf112ce67fd854a7
Miguel <rgomiguel17@gmail.com>
2024-03-23 15:16:44 -0300

    Remoção de titulo e usando back ao invez de redux

--- src/Mostrador/Mostrador.js
+++ src/Mostrador/Mostrador.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import parse from "html-react-parser";
 import './Mostrador.css';
 
-import { useSelector } from 'react-redux';
-import  {selectTexts}  from '../basico/TextsSlice.js';
 import { Link } from 'react-router-dom';
+import BackComunication from '../basico/ComunicationBack.js';
 
 function Mostrador(props) {
+  const [chatInfo, setChatInfo] = useState("");
+  useEffect(() => {
+    BackComunication.getChatWithLastMessage().then( (res) =>{
+      console.log("aaa", res);
+      setChatInfo(res);
+    });
+  }, [])
+
   function getContrastYIQ(hexcolor){
     var r = parseInt(hexcolor.substr(0,2),16);
     var g = parseInt(hexcolor.substr(2,2),16);
@@ -14,35 +21,23 @@ function Mostrador(props) {
     var yiq = ((r*299)+(g*587)+(b*114))/1000;
     return (yiq >= 128) ? 'black' : 'white';
   }
-  const texts = useSelector(selectTexts);
 
-  const texts_in_divs = [];
-  console.log(texts)
-  Object.keys(texts).forEach((step, index) => {
-    let my_dict = texts[step];
-    let last_key = Object.keys(texts[step])[Object.keys(texts[step]).length - 1];
-    console.log("stpe", step);
-    texts_in_divs.push(
-
+  const chat_in_divs = [];
+  Object.keys(chatInfo).forEach((step, index) => {
+    let chat_info = chatInfo[step];
+    chat_in_divs.push(
       <Link key={index}
-        to={{
-            
-            pathname:"/editor"}} state={index + 1}>
-        <p  className='divTextCaracteristics' style={{color: getContrastYIQ(my_dict["color"]), backgroundColor: "#"+my_dict["color"]}}>
-          {parse(my_dict[last_key])}
+        to={{pathname:"/editor"}} state={index + 1}>
+        <p  className='divTextCaracteristics' style={{color: getContrastYIQ(chat_info["color"]), backgroundColor: chat_info["color"]}}>
+          {parse(chat_info["text"])}
         </p>
       </Link>
     );
   });
 
   return (
-    <div>
-      <div id="todoMostrador">
-        <div id='posTitulo'>
-          <h1 id='my-title'>Title</h1>
-        </div>
-        <div id='organizeDiv'>{texts_in_divs}</div>
-      </div>
+    <div id="todoMostrador">
+      <div id='organizeDiv'>{chat_in_divs}</div>
 
       <footer className='rodape'>
         <a href='https://elmigu17.github.io/portifolio/' className='link-my-page'>Made By Miguel</a>
