4d4dad48de8bbd21da6b65129b7675b2ab0ae07c
Miguel <rgomiguel17@gmail.com>
2024-03-24 17:51:38 -0300

    remoção do textSlice

--- src/store.js
+++ src/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
-import textsReducer from './basico/TextsSlice.js'
 
 export default configureStore({
   reducer: {
-    texts: textsReducer,
   },
-})
+}) 
